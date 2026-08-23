53834563a361c90df0d5509b356a12f48a55e053
Vaishakh Suresh <vaishakh@Vaishakhs-MacBook-Pro.local>
2026-08-24 01:29:55 +0530

    Revert "Create Kochi Buzz launch film"
    This reverts commit dd7aace4a2a60fd18daa52c15473d8b408abd0ff.

--- launch-video/remotion.config.ts
+++
@@ -1,8 +0,0 @@
-import {Config} from "@remotion/cli/config";
-
-Config.setOverwriteOutput(true);
-Config.setPixelFormat("yuv420p");
-// PNG intermediates avoid full-range JPEG color metadata leaking into the H.264
-// master. The final encode stays broadcast-safe BT.709-compatible yuv420p.
-Config.setVideoImageFormat("png");
-Config.setConcurrency(4);
